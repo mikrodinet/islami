fe7d8bf1d6710b1501c08a3b0aebc386775ad9c0
AWDEV CORPORATION <84139744+wahyu9kdl@users.noreply.github.com>
2022-07-13 14:18:15 +0700

    Add files via upload

---
+++ adzkar/README.md
@@ -0,0 +1,11 @@
+# Athkar Al Muslim
+
+### This website will help organize all different athkar according to the timing categories they are supposed to be recited, with digital tasbeeh counter included to count/tally tasbeeh portions. The ***prayer times and muslim holidays*** are also included in the home page. 
+
+* Athkar Al Sabah (Morning Athkar)
+* Athkar Al Masaa (Evening Athkar)
+* Athkar after obligatory prayers
+* Athkar before sleeping and wake up
+### ....and more
+
+#### Website address: https://samsal81.github.io/athkaralmuslim/
---
+++ adzkar/scripts.js
@@ -0,0 +1,187 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+var clicks1 = document.getElementById("clicks1").innerHTML;
+
+function onClick1() {
+    if (clicks1 > 0) {
+        clicks1 -= 1;
+        document.getElementById("clicks1").innerHTML = clicks1;
+    }
+};
+
+var clicks2 = document.getElementById("clicks2").innerHTML;
+
+function onClick2() {
+    if (clicks2 > 0) {
+        clicks2 -= 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    }
+};
+
+var clicks3 = document.getElementById("clicks3").innerHTML;
+
+function onClick3() {
+    if (clicks3 > 0) {
+        clicks3 -= 1;
+        document.getElementById("clicks3").innerHTML = clicks3;
+    }
+};
+
+var clicks4 = document.getElementById("clicks4").innerHTML;
+
+function onClick4() {
+    if (clicks4 > 0) {
+        clicks4 -= 1;
+        document.getElementById("clicks4").innerHTML = clicks4;
+    }
+};
+
+var clicks5 = document.getElementById("clicks5").innerHTML;
+
+function onClick5() {
+    if (clicks5 > 0) {
+        clicks5 -= 1;
+        document.getElementById("clicks5").innerHTML = clicks5;
+    }
+};
+
+var clicks6 = document.getElementById("clicks6").innerHTML;
+
+function onClick6() {
+    if (clicks6 > 0) {
+        clicks6 -= 1;
+        document.getElementById("clicks6").innerHTML = clicks6;
+    }
+};
+
+var clicks7 = document.getElementById("clicks7").innerHTML;
+
+function onClick7() {
+    if (clicks7 > 0) {
+        clicks7 -= 1;
+        document.getElementById("clicks7").innerHTML = clicks7;
+    }
+};
+
+var clicks8 = document.getElementById("clicks8").innerHTML;
+
+function onClick8() {
+    if (clicks8 > 0) {
+        clicks8 -= 1;
+        document.getElementById("clicks8").innerHTML = clicks8;
+    }
+};
+
+var clicks9 = document.getElementById("clicks9").innerHTML;
+
+function onClick9() {
+    if (clicks9 > 0) {
+        clicks9 -= 1;
+        document.getElementById("clicks9").innerHTML = clicks9;
+    }
+};
+
+var clicks10 = document.getElementById("clicks10").innerHTML;
+
+function onClick10() {
+    if (clicks10 > 0) {
+        clicks10 -= 1;
+        document.getElementById("clicks10").innerHTML = clicks10;
+    }
+};
+
+var clicks11 = document.getElementById("clicks11").innerHTML;
+
+function onClick11() {
+    if (clicks11 > 0) {
+        clicks11 -= 1;
+        document.getElementById("clicks11").innerHTML = clicks11;
+    }
+};
+
+var clicks12 = document.getElementById("clicks12").innerHTML;
+
+function onClick12() {
+    if (clicks12 > 0) {
+        clicks12 -= 1;
+        document.getElementById("clicks12").innerHTML = clicks12;
+    }
+};
+
+var clicks13 = document.getElementById("clicks13").innerHTML;
+
+function onClick13() {
+    if (clicks13 > 0) {
+        clicks13 -= 1;
+        document.getElementById("clicks13").innerHTML = clicks13;
+    }
+};
+
+var clicks14 = document.getElementById("clicks14").innerHTML;
+
+function onClick14() {
+    if (clicks14 > 0) {
+        clicks14 -= 1;
+        document.getElementById("clicks14").innerHTML = clicks14;
+    }
+};
+
+var clicks15 = document.getElementById("clicks15").innerHTML;
+
+function onClick15() {
+    if (clicks15 > 0) {
+        clicks15 -= 1;
+        document.getElementById("clicks15").innerHTML = clicks15;
+    }
+};
+
+var clicks16 = document.getElementById("clicks16").innerHTML;
+
+function onClick16() {
+    if (clicks16 > 0) {
+        clicks16 -= 1;
+        document.getElementById("clicks16").innerHTML = clicks16;
+    }
+};
+
+var clicks17 = document.getElementById("clicks17").innerHTML;
+
+function onClick17() {
+    if (clicks17 > 0) {
+        clicks17 -= 1;
+        document.getElementById("clicks17").innerHTML = clicks17;
+    }
+};
+
+var clicks18 = document.getElementById("clicks18").innerHTML;
+
+function onClick18() {
+    if (clicks18 > 0) {
+        clicks18 -= 1;
+        document.getElementById("clicks18").innerHTML = clicks18;
+    }
+};
+
+var clicks19 = document.getElementById("clicks19").innerHTML;
+
+function onClick19() {
+    if (clicks19 > 0) {
+        clicks19 -= 1;
+        document.getElementById("clicks19").innerHTML = clicks19;
+    }
+};
+
+var clicks20 = document.getElementById("clicks20").innerHTML;
+
+function onClick20() {
+    if (clicks20 > 0) {
+        clicks20 -= 1;
+        document.getElementById("clicks20").innerHTML = clicks20;
+    }
+};
