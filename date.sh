a0acee81a4557007c177a4e81b4b3b6dcb8990a4
AWDEV CORPORATION <84139744+wahyu9kdl@users.noreply.github.com>
2022-08-01 07:11:57 +0700

    Create date.sh

---
+++ date.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DATE="$(date)"
+echo "Content-type: text/html"
+echo ""
+echo "    
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="shortcut icon" type="image/x-icon" href="https://wahyu9kdl.github.io/islami/kitab.png">
+<title>KUMPULAN KITAB KUNING LENGKAP - APLIKASI ISLAMI </title>
+<meta name="keywords" content="KITAB KUNING,WEBSITE ISLAMI APLIKASI ISLAMI,AWDEV ISLAMI,APLILASI ISLAMI,PRODUCTS AWDEV" />
+<meta name="description" content="Kumpulan Kitab Kuning Lengkap | Aplikasi Islami | Proges Web App Islami Deseign AWDEV"/>
+<link rel="canocial" href="https://wahyu9kdl.github.io/islami/index.html">
+</head><body>"
+echo "Today is $DATE <br>"
